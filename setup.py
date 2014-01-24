bde4c6f489cc302b2aa04f1af0a1b809ede7bc82
bufferx (Zhang ZY) <idup2x@gmail.com>
2014-01-24 15:31:19 +0800

    Update package name

--- setup.py
+++ setup.py
@@ -33,14 +33,15 @@ if "linux" in sys.platform.lower() and not python_26:
     extensions.append(distutils.core.Extension(
         "tornado.epoll", ["tornado/epoll.c"]))
 
-version = "2.4.1"
+import tornado
+version = tornado.version
 
 if major >= 3:
     import setuptools  # setuptools is required for use_2to3
     kwargs["use_2to3"] = True
 
 distutils.core.setup(
-    name="tornado",
+    name="tornado-xspeed",
     version=version,
     packages = ["tornado", "tornado.test", "tornado.platform"],
     package_data = {
